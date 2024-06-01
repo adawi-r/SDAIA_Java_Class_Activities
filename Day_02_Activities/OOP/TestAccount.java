e3ea7e6c7fed450b521dd8fc7ff1e40a17ac7946
Raghad Mohammed Aladawi <109043585+adawi-r@users.noreply.github.com>
2024-06-01 15:59:19 +0300

    Day_02_Class_Activities

---
+++ Day_02_Activities/OOP/TestAccount.java
@@ -0,0 +1,22 @@
+package Day2.OOP;
+
+public class TestAccount {
+
+    public static void main(String[] args) {
+
+        Account a1 = new Account("Raghad", 2000);
+
+        Account.annualRate = 0.04;
+        System.out.println(a1.name);
+
+        System.out.println(a1.getBalance());
+        a1.withDraw(500);
+        a1.deposit(1000);
+
+        System.out.println(a1.getBalance());
+        System.out.println(a1.getMonthlyRate());
+        a1.deposit(500);
+        a1.withDraw(500);
+
+    }
+}
