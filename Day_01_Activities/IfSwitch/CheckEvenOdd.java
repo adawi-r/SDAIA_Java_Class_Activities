b4a44ffb6eb8f3c038fc36a8bbc0479f2cdf30f8
Raghad Mohammed Aladawi <109043585+adawi-r@users.noreply.github.com>
2024-06-02 08:31:24 +0300

    Day_01_Activities

---
+++ Day_01_Activities/IfSwitch/CheckEvenOdd.java
@@ -0,0 +1,17 @@
+package Day1.Day_01_Activities.IfSwitch;
+
+import java.util.Scanner;
+
+public class CheckEvenOdd {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a day number");
+
+        int num = input.nextInt();
+
+        // TRUE = Even , F = Odd
+        System.out.println(num % 2 == 0 ? "Even" : "Odd");
+
+    }
+}
---
+++ Day_01_Activities/IfSwitch/RandomNum.java
@@ -0,0 +1,27 @@
+package Day1.Day_01_Activities.IfSwitch;
+
+import java.util.Scanner;
+
+public class RandomNum {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a number");
+
+        int x = (int) (Math.random() * 10);
+        int y = (int) (Math.random() * 10);
+
+//        System.out.println("What's" + x + "*" + y + "?"); or
+        System.out.printf("What's %d * %d", x,y);
+
+        int answer = input.nextInt();
+
+        if(answer == x*y) {
+            System.out.println("Correct");
+        }
+        else {
+            System.out.println("Wrong");
+        }
+
+    }
+}
